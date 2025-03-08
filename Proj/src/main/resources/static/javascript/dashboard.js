c5961c3e8cb1ad8f263d5e14a09756d8eae26623
Scott-MASE <a00325403@student.tus.ie>
2025-03-08 22:03:35 +0000

    Working on modal for note creation

--- Proj/src/main/resources/static/javascript/dashboard.js
+++ Proj/src/main/resources/static/javascript/dashboard.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
 
 	
 	
-console.log("hello");
+	$('#create-note').off('click').on('click', function(e) {
+	    e.preventDefault(); // Prevent default behavior
+	    console.log("Opening modal...");
+	    $('#createNoteModal').modal('show'); // Open Bootstrap modal
+	});
 	
 	
 });
