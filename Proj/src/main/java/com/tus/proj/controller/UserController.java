a37103a713ea8efc013b69d93dd8543b276fce84
Scott-MASE <a00325403@student.tus.ie>
2025-03-31 13:03:05 +0100

    final hopefully

--- Proj/src/main/java/com/tus/proj/controller/UserController.java
+++ Proj/src/main/java/com/tus/proj/controller/UserController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tus.proj.service.JwtService;
-import com.tus.proj.service.NoteService;
 import com.tus.proj.service.UserService;
 import com.tus.proj.user_managment.User;
 import com.tus.proj.user_managment.UserRole;
@@ -36,7 +35,7 @@ public class UserController {
     private final UserService userService;
     private final JwtService jwtService;
 
-    public UserController(UserService userService, NoteService noteService,JwtService jwtService) {
+    public UserController(UserService userService,JwtService jwtService) {
         this.userService = userService;
         this.jwtService = jwtService;
     }
